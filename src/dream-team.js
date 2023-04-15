c9e3ed03d1d332e5d35dc7167bfa75845d2bb51b
Peter Terentev <petrfalkovsky@yandex.ru>
2023-04-15 16:36:05 +0600

    feat: add two tsks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,21 +1,29 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  const newMembers = members
+    .filter((item) => typeof item === "string")
+    .map((el) => el.trim())
+    .map((el) => el[0])
+    .sort((a, b) => a.localeCompare(b))
+    .join("")
+    .toLocaleUpperCase();
+
+  return newMembers;
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,10 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let count = 1;
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      result += count + str[i];
+      count = 1;
+    }
+  }
+  return result.replace(/1/g, "");
 }
+
+// = 4a4t
+// console.log(encodeLine());
 
 module.exports = {
   encodeLine
