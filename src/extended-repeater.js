89b0cb31aec4e81b4482fa95a72126b1c3cb6687
Peter Terentev <petrfalkovsky@yandex.ru>
2023-04-16 16:39:36 +0600

    feat: added two more

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,23 +1,35 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1;
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+
+  const separator = options.separator || "+";
+  const additionSeparator = options.additionSeparator || "|";
+
+  const addition =
+    options.addition === undefined ? "" : String(options.addition);
+  // важен порядок для инициализации
+  const repeatedAdditionArr = new Array(additionRepeatTimes).fill(addition);
+  const repeatedAddition = repeatedAdditionArr.join(additionSeparator);
+  const repeatedArr = new Array(repeatTimes).fill(str + repeatedAddition);
+
+  return repeatedArr.join(separator);
 }
 
 module.exports = {
--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,9 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  // const names = ["doc", "doc", "image", "doc(1)", "doc"];
+  // ['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)']);
+  const nameCounts = {};
+  const uniqueNames = [];
+
+  for (const name of names) {
+    if (!nameCounts.hasOwnProperty(name)) {
+      nameCounts[name] = 0;
+      uniqueNames.push(name);
+    } else {
+      let newName = name + `(${nameCounts[name] + 1})`;
+      while (nameCounts.hasOwnProperty(newName)) {
+        newName = name + `(${++nameCounts[name] + 1})`;
+      }
+      nameCounts[newName] = 0;
+      uniqueNames.push(newName);
+    }
+  }
+
+  return uniqueNames;
 }
 
 module.exports = {
